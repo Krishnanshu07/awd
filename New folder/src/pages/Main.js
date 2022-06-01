131de94f306903df2261b976ee5bffd9ef342ee7
Krishnanshu07 <102716730+Krishnanshu07@users.noreply.github.com>
2022-06-01 09:14:28 +0530

    Add files via upload

---
+++ New folder/src/pages/Main.js
@@ -0,0 +1,193 @@
+import React from 'react'
+import '../pages/Main.css'
+
+
+function Main(){
+    return (
+        <>
+        <div>
+        <>
+        
+   
+    return (
+      <div>
+        
+        
+        <nav id="navbar">
+          <img src="https://user-images.githubusercontent.com/102716730/171253867-32b8e300-b5ed-40e1-86e2-d2dd6ae9ef20.jpg" alt="krishnanshu" />
+          <ul>
+            <li className="item"><a href="#contact">Drop Your Poetry</a></li>
+          </ul>
+        </nav>
+        <section id="home">
+          <h1 className="h-primary">Welcome to Shayarana-andaaz</h1>
+          <p style={{fontSize: 'medium'}}>nihaa.n
+            ( निहाँ )<br />
+            means
+            hidden/secret<br />
+            <br />
+            <br />
+            aatā huuñ maiñ zamāne kī āñkhoñ meñ raat din
+            <br />
+            <br /> 
+            lekin ḳhud apnī nazroñ se ab tak nihāñ huuñ maiñ 
+          </p>
+        </section>
+        <section id="services-container">
+          <h1 className="h-primary center">Some Famous Shayars</h1>
+          <div id="services">
+            <div className="box">
+              <img src="https://user-images.githubusercontent.com/102716730/171247324-6ea8648c-a13d-4298-8b75-0328b2d337cf.jpg" alt="Krishnanshu" />
+              <h2 className="h-secondary center">Ahmed Faraz Sahab</h2>
+              <p className="center">Faraz began his poetry career during his college life with the Tanha Tanha ghazal collection. Love and beauty were the main themes of his poetry. His poems were used by the singers, including Mehdi Hassan who sung Ranjish he sahi ghazal. His poem Silsilay Tor Gaya was sung by Noor Jahan.
+              </p>
+            </div>
+            <div className="box">
+              <img src="https://user-images.githubusercontent.com/102716730/171247449-e7b1215b-62fa-466e-ac45-4061334f7b7d.jpg" alt="Krishnanshu" />
+              <h2 className="h-secondary center">Jaun Elia Sahab</h2>
+              <p className="center">Jaun Elia. One of the most popular modern shaayar of Pakistan. Jaun Elia Shayari has become popular a lot in the last few years. The number of fans of Jaun Elia poetry is increasing as well. Jaun Elia poetry is sad and alone.</p>
+            </div>
+            <div className="box">
+              <img src="https://user-images.githubusercontent.com/102716730/171247377-d6c97c15-55c2-4881-b89d-dee294c32c44.jpg" alt="Krishnanshu" />
+              <h2 className="h-secondary center">Gulzar Sahab</h2>
+              <p className="center">Sampooran Singh Kalra (born 18 August 1934), known professionally as Gulzar or Gulzar Saab , is an Indian lyricist, poet, author, screenwriter, and film director.[1] He started his career with music director S.D. Burman as a lyricist in the 1963 film Bandini and worked with many music directors including R. D. Burman, Salil Chowdhury, Vishal Bhardwaj and A. R. Rahman. He was awarded Padma Bhushan in 2004, the third-highest civilian award in India,[2] the Sahitya Academy Award, and the Dadasaheb Phalke Award — the highest award in Indian cinema. He has won 5 Indian National Film Awards, 22 Filmfare Awards, one Academy Award and one Grammy Award.[3][4].</p>
+            </div>
+            <div className="box">
+              <img src="https://user-images.githubusercontent.com/102716730/171247416-7e73aff0-e3a3-4c8a-9725-35306d746e9c.jpg" alt="Krishnanshu" />
+              <h2 className="h-secondary center">Javed Akhtar</h2>
+              <p className="center">Javed Akhtar (born 17 January 1945) is an Indian poet, lyricist, screenwriter and political activist. Known for his work in Hindi cinema, he has won five National Film Awards,[1] and received the Padma Shri in 1999 and the Padma Bhushan in 2007,[2] two of India's highest civilian honours.</p>
+            </div>
+          </div>
+        </section>
+        <section id="Shayaris-container">
+          <h1 className="h-primary center">Shayars and their Art!</h1>
+          <div id="shayaris">
+            <div className="box">
+              <img src="https://user-images.githubusercontent.com/102716730/171247360-53e3a2dc-59cc-4dc1-9cb1-4fbc103ab551.jpg" alt="krishnanshu" />
+              <h2 className="h-secondary center">Ahmad Faraz </h2>
+              <p className="center">ranjish hī sahī dil hī dukhāne ke liye aa <br />
+                aa phir se mujhe chhoḌ ke jaane ke liye aa <br />
+                kuchh to mire pindār-e-mohabbat kā bharam rakh <br />
+                tū bhī to kabhī mujh ko manāne ke liye aa <br />
+                pahle se marāsim na sahī phir bhī kabhī to <br />
+                rasm-o-rah-e-duniyā hī nibhāne ke liye aa <br />
+                kis kis ko batā.eñge judā.ī kā sabab ham <br />
+                tū mujh se ḳhafā hai to zamāne ke liye aa <br />
+                ik umr se huuñ lazzat-e-girya se bhī mahrūm <br />
+                ai rāhat-e-jāñ mujh ko rulāne ke liye aa <br />
+                ab tak dil-e-ḳhush-fahm ko tujh se haiñ ummīdeñ<br /> 
+                ye āḳhirī sham.eñ bhī bujhāne ke liye aa </p>
+            </div>
+            <div className="box">
+              <img src="https://user-images.githubusercontent.com/102716730/171246026-c0109d2b-e055-4a11-baee-b39eab7ba5f8.jpg" alt="krishnanshu" />
+              <h2 className="h-secondary center">Javed Akhtar</h2>
+              <p className="center">kabhī kabhī maiñ ye sochtā huuñ ki mujh ko terī talāsh kyuuñ hai <br />
+                ki jab haiñ saare hī taar TuuTe to saaz meñ irti.āsh kyuuñ hai <br />
+                koī agar pūchhtā ye ham se batāte ham gar to kyā batāte <br />
+                bhalā ho sab kā ki ye na pūchhā ki dil pe aisī ḳharāsh kyuuñ hai<br /> 
+                uThā ke hāthoñ se tum ne chhoḌā chalo na dānista tum ne toḌā <br />
+                ab ulTā ham se to ye na pūchho ki shīsha ye paash paash kyuuñ hai<br /> 
+                ajab do-rāhe pe zindagī hai kabhī havas dil ko khīñchtī hai <br />
+                kabhī ye sharmindagī hai dil meñ ki itnī fikr-e-ma.āsh kyuuñ hai<br /> 
+                na fikr koī na justujū hai na ḳhvāb koī na aarzū hai <br />
+                ye shaḳhs to kab kā mar chukā hai to be-kafan phir ye laash kyuuñ hai<br /> </p>
+            </div>
+            <div className="box">
+              <img src="https://user-images.githubusercontent.com/102716730/171247449-e7b1215b-62fa-466e-ac45-4061334f7b7d.jpg" alt="krishnanshu" />
+              <h2 className="h-secondary center">Jaun Elia</h2>
+              <p className="center"> umr guzregī imtihān meñ kyā <br />
+                daaġh hī deñge mujh ko daan meñ kyā<br /> 
+                merī har baat be-asar hī rahī <br />
+                naqs hai kuchh mire bayān meñ kyā <br />
+                mujh ko to koī Toktā bhī nahīñ <br />
+                yahī hotā hai ḳhāndān meñ kyā <br />
+                apnī mahrūmiyāñ chhupāte haiñ <br />
+                ham ġharīboñ kī ān-bān meñ kyā <br />
+                ḳhud ko jaanā judā zamāne se <br />
+                aa gayā thā mire gumān meñ kyā <br />
+                shaam hī se dukān-e-dīd hai band <br />
+                nahīñ nuqsān tak dukān meñ kyā <br />
+                ai mire sub.h-o-shām-e-dil kī shafaq<br /> 
+                tū nahātī hai ab bhī baan meñ kyā <br />
+                bolte kyuuñ nahīñ mire haq meñ <br />
+                aable paḌ ga.e zabān meñ kyā <br />
+                ḳhāmushī kah rahī hai kaan meñ kyā<br /> 
+                aa rahā hai mire gumān meñ kyā <br />
+                dil ki aate haiñ jis ko dhyān bahut<br /> 
+                ḳhud bhī aatā hai apne dhyān meñ kyā <br />
+                vo mile to ye pūchhnā hai mujhe <br />
+                ab bhī huuñ maiñ tirī amaan meñ kyā <br />
+                yuuñ jo taktā hai āsmān ko tū <br />
+                koī rahtā hai āsmān meñ kyā <br />
+                hai nasīm-e-bahār gard-ālūd <br />
+                ḳhaak uḌtī hai us makān meñ kyā <br />
+                ye mujhe chain kyuuñ nahīñ paḌtā <br />
+                ek hī shaḳhs thā jahān meñ kyā </p>
+            </div>
+            <div className="box">
+              <img src="https://user-images.githubusercontent.com/102716730/171247377-d6c97c15-55c2-4881-b89d-dee294c32c44.jpg" alt="krishnanshu" />
+              <h2 className="h-secondary center">Gulzar Sahab</h2>
+              <p className="center"> ek parvāz dikhā.ī dī hai <br />
+                terī āvāz sunā.ī dī hai <br />
+                sirf ik safha palaT kar us ne<br /> 
+                saarī bātoñ kī safā.ī dī hai <br />
+                phir vahīñ lauT ke jaanā hogā <br />
+                yaar ne kaisī rihā.ī dī hai <br />
+                jis kī āñkhoñ meñ kaTī thiiñ sadiyāñ <br />
+                us ne sadiyoñ kī judā.ī dī hai <br />
+                zindagī par bhī koī zor nahīñ <br />
+                dil ne har chiiz parā.ī dī hai <br />
+                aag meñ kyā kyā jalā hai shab bhar <br />
+                kitnī ḳhush-rañg dikhā.ī dī hai </p>
+            </div>
+          </div>
+        </section>
+        <section id="Urdu Dictionary" ><h1 className="h-primary center">Urdu Dictionary</h1>
+          <section id="contact">
+            <h1 className="h-primary center">Drop Your Poetry</h1>
+            <div id="contact-box">
+              <form action>
+                <div className="form-group">
+                  <label htmlFor="name">Name: </label>
+                  <input type="text" name="name" id="name" placeholder="Enter your name" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email: </label>
+                  <input type="email" name="name" id="email" placeholder="Enter your email" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="phone">Phone Number: </label>
+                  <input type="phone" name="name" id="phone" placeholder="Enter your phone" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="message">Drop Your Shayari Here </label>
+                  <textarea name="message" id="message" cols={30} rows={10} defaultValue={""} />
+                </div>
+              </form>
+            </div>
+          </section>
+          <section id="animation">
+            <div className="wrapper">
+              <div className="cube-box">
+                <img src="https://user-images.githubusercontent.com/102716730/171246026-c0109d2b-e055-4a11-baee-b39eab7ba5f8.jpg" alt="krishnanshu" />
+                <img src="https://user-images.githubusercontent.com/102716730/171247324-6ea8648c-a13d-4298-8b75-0328b2d337cf.jpg" alt="krishnanshu" />
+                <img src="https://user-images.githubusercontent.com/102716730/171247377-d6c97c15-55c2-4881-b89d-dee294c32c44.jpg" alt="krishnanshu" />
+                <img src="https://user-images.githubusercontent.com/102716730/171247416-7e73aff0-e3a3-4c8a-9725-35306d746e9c.jpg" alt="krishnanshu" />
+                <img src="https://user-images.githubusercontent.com/102716730/171247427-9f48dce6-ef35-45f4-a3b7-4f3e7e6d11a5.jpg" alt="krishnanshu" />
+                <img src="https://user-images.githubusercontent.com/102716730/171247449-e7b1215b-62fa-466e-ac45-4061334f7b7d.jpg" alt="krishnanshu" />
+              </div>
+            </div></section>
+          <footer>
+            <div className="center">
+              All rights reserved! made by lakshay sharma ,krishnanshu bhanwar,lehardeep singh,kriti jain
+            </div>
+          </footer>
+        </section></div>
+    );
+
+        </>
+        </div>
+        </>
+    )
+}
+export default Main
